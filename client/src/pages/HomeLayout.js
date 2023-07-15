146c165fcae844e2af288efc0e92cb9cc73a8e0a
Sobhi <omar_sobhi_finalproject12@outlook.com>
2023-07-15 23:13:18 +0300

    Fixed responsivness

--- client/src/pages/HomeLayout.js
+++ client/src/pages/HomeLayout.js
@@ -2,12 +2,16 @@ import React from 'react'
 import FirstSection from '../components/FirstSection/FirstSection';
 import ThirdSection from '../components/ThirdSection/ThirdSection';
 import Header from '../components/Header/Header';
+import classes from './homelayout.module.css';
+
 export default function HomeLayout({openLogin, openRegister}) {
   return (
     <>
-       <Header openLogin={openLogin} openRegister={openRegister}/>
-       <FirstSection/>
-       <ThirdSection/>
+       <main className={classes.main}>
+        <Header openLogin={openLogin} openRegister={openRegister}/>
+        <FirstSection/>
+        <ThirdSection/>
+       </main>
     </>
   )
 }
